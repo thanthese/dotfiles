9d66ec9dd2e901435686d7387c354af0e2319f0d
Stephen Mann <stephen.mann@geocent.com>
2011-09-13 14:36:37 -0500

    spelling shortcuts

--- thanthese/custom.el
+++ thanthese/custom.el
@@ -1,11 +1,15 @@
+;;; require additional plugins
+
 ;; vim
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
 
-;; surround
+;; vim-style surround
 (require 'surround)
 (global-surround-mode 1)
+
+;;; don't require additional plugins
 
 ;; highlight lines longer than 80 chars
 (require 'whitespace)
@@ -13,6 +17,11 @@
                                   indentation space-after-tab)
       whitespace-Line-column 80)
 (global-whitespace-mode 1)
+
+;; spelling correct
+(add-hook 'text-mode-hook
+          (lambda () (flyspell-mode 1)))
+(global-set-key (kbd "C->") 'flyspell-auto-correct-word)
 
 ;; number lines
 (global-linum-mode 1)
